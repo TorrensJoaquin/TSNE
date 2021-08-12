48356d4bc6b646e924402c30eee5df7eb60de0bd
Joaquín Torrens <71098233+TorrensJoaquin@users.noreply.github.com>
2021-08-11 23:43:02 -0300

    Update 11_Inputs.js

--- 11_Inputs.js
+++ 11_Inputs.js
@@ -2,32 +2,32 @@ let inpPerplexity;
 let inpTradeOff;
 function CreateTheInputs(){
     let SizeOfInput=[60,20];
-    inpPerplexity=createInput((3).toString());
+    inpPerplexity=createInput((DesiredPerplexity).toString());
     inpPerplexity.size(SizeOfInput[0],SizeOfInput[1]);
     inpPerplexity.position(370,39);
     inpPerplexity.input(myInputPerplexity);
 
-    inpLearningRatio=createInput((3).toString());
+    inpLearningRatio=createInput((LearningRatio).toString());
     inpLearningRatio.size(SizeOfInput[0],SizeOfInput[1]);
     inpLearningRatio.position(626,25);
     inpLearningRatio.input(myInputLearningRatio);
 
-    inpMomentum=createInput((0.3).toString());
+    inpMomentum=createInput((Momentum).toString());
     inpMomentum.size(SizeOfInput[0],SizeOfInput[1]);
     inpMomentum.position(626,52);
     inpMomentum.input(myInputMomentum);
 
-    inpTradeOff=createInput((0.5).toString());
+    inpTradeOff=createInput((TradeOff).toString());
     inpTradeOff.size(SizeOfInput[0],SizeOfInput[1]);
     inpTradeOff.position(810,39);
     inpTradeOff.input(myInputTradeOff);
 
-    inpEarlyExaggerationIterations=createInput((250).toString());
+    inpEarlyExaggerationIterations=createInput((EarlyExaggeration.Iterations).toString());
     inpEarlyExaggerationIterations.size(SizeOfInput[0],SizeOfInput[1]);
     inpEarlyExaggerationIterations.position(1007,25);
     inpEarlyExaggerationIterations.input(myInputEarlyExaggerationIterations);
 
-    inpEarlyExaggerationFactor=createInput((4).toString());
+    inpEarlyExaggerationFactor=createInput((EarlyExaggeration.Factor).toString());
     inpEarlyExaggerationFactor.size(SizeOfInput[0],SizeOfInput[1]);
     inpEarlyExaggerationFactor.position(1007,52);
     inpEarlyExaggerationFactor.input(myInputEarlyExaggerationFactor);
